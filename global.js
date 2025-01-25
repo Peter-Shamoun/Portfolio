84e61866ad41271e8ed3271a8e4fb81853967a9a
Peter Shamoun <petershamoun80@gmail.com>
2025-01-24 19:56:36 -0800

    2.2

--- global.js
+++ global.js
@@ -5,3 +5,8 @@ function $$(selector, context = document) {
 }
 
 const navLinks = $$("nav a");
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+console.log('Current link:', currentLink); // For debugging
